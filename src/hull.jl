a8bd1088790e7f5b7c54ac17e86edf82c060edc3
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2021-03-05 23:30:52 +0000

    Added convergence flag to hull. (#15)

--- src/hull.jl
+++ src/hull.jl
@@ -1,9 +1,10 @@
 struct Hull{T<:AbstractVector}
     vertices::Vector{T}
     k::Int
+    converged::Bool
 end
 
-Hull(::Type{T},k::Int) where {T<:AbstractVector} = Hull(T[],k)
+Hull(::Type{T},k::Int) where {T<:AbstractVector} = Hull(T[],k, false)
 
 function is_left(p0,p1,p2)
     return ((p1[1] - p0[1]) * (p2[2] - p0[2]) - (p2[1] -  p0[1]) * (p1[2] - p0[2]))
